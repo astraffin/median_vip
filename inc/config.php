cea8096912d4dc53e348dbeadacf7df85cf2f1d5
astraffin <alex.straffin@gmail.com>
2012-11-19 20:35:00 -0500

    dbpass sorted

---
+++ inc/config.php
@@ -0,0 +1,15 @@
+<!-- CONFIG FILE -->
+<?php
+
+// Database Config: Replace info inside "" with your database info.
+$db_server = "localhost";
+$db_user = "root";
+$db_pass = "password";
+$db_name = "reg";
+
+
+
+
+
+?>
+<!-- END CONFIG FILE -->
